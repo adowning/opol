fbd5188bad770fb808af86082b4d09b175ff5934
ashdowning@gmail.com <ashdowning@gmail.com>
2018-01-08 01:14:13 -0600

    adsf

--- rocketChat/rocketChat.js
+++ rocketChat/rocketChat.js
@@ -5,48 +5,9 @@ var bodyParser = require("body-parser")
 var app = express()
 app.use(bodyParser.json())
 
-// const config = {
-//     protocol: "http",
-//     host: "vultr.ashdevtools.com",
-//     port: "3000",
-//     username: "ash",
-//     password: "sugarlips"
-// }
-
-// rocketChatClient = new RocketChatClient(config)
-// rocketChatClient.authentication.me(function(err, body) {
-//     console.log(err)
-//     console.log(body)
-// })
 var rocketChatApi = null
-    // var rocketChatApi = new RocketChatApi(
-    //     "http",
-    //     "vultr.ashdevtools.com",
-    //     3000,
-    //     "ash",
-    //     "sugarlips42"
-    // )
 
-// rocketChatApi.login(config.username, config.password, function(err, body) {
-//     console.log(body)
-//     rocketChatApi.sendMsg("GENERAL", "HAI", function(err, body) {
-//         console.log(err)
-//     })
-// })
 sendmessage = function(rocketChatClient, req) {
-    // console.log("recieved " + req.body.channel)
-    // console.log("recieved " + req.body.user)
-    // var rocketChatApi = null
-
-    // rocketChatApi = new RocketChatApi(
-    //     "http",
-    //     config.host,
-    //     config.port,
-    //     "ash",
-    //     "sugarlips"
-    // )
-
-    // rocketChatApi.joinRoom(req.body.channel, function(err) {})
     rocketChatClient.chat.postMessage({ roomId: req.body.channel, text: req.body.msg },
         function(err) {
             console.log(err)
@@ -75,8 +36,6 @@ app.post("/sendmsg", (req, res) => {
             this.sendmessage(rocketChatClient, req)
         }
     })
-
-    // this.sendmessage(req)
 })
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"))
